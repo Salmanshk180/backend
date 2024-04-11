a35a0f429a5da43ab7bef61bf6cd4ab16de60436
Salman Shaikh <salmanshaikh7118@gmail.com>
2024-04-11 22:50:51 +0530

    remove id from product route

--- src/services/products/get-product.services.ts
+++ src/services/products/get-product.services.ts
@@ -3,22 +3,29 @@ import { ProductVariants } from "../../entities/products/product-variants.entity
 import { ERROR_MESSAGE } from "../../utils/constants/error-message.constants";
 import { HTTP_STATUS_CODES } from "../../utils/constants/status-codes.constants";
 import { SUCCESS_MESSAGES } from "../../utils/constants/success-message.constants";
+import { SizeType } from "../../types/types";
 
 export const getProduct = async (req: Request) => {
   try {
-    const id = req.params.id;
-    const product = await ProductVariants.findOne({
-      where: { id: id },
-    });
-    const product_variants = await ProductVariants.find({
-      where: { product: { id: product?.product.id } },
-    });
+    const color = req.query.color?.toString();
+    const size = req.query.size?.toString();    
+    const product_id = req.query.productid?.toString();    
+    let product = await ProductVariants.findOne({
+      where: {product:{id:product_id},color: color},
+    });    
+    if(size !=="undefined"){
+       product = await ProductVariants.findOne({
+        where: {product:{id:product_id},color: color,size: size as SizeType},
+      });
+
+    }
+    const productVariants = await ProductVariants.find({where:{product:{id: product_id}}})
     return {
       statusCode: HTTP_STATUS_CODES.OK,
       message: SUCCESS_MESSAGES._Ok("Product Variants"),
       data: {
         product: product,
-        product_variants: product_variants,
+        product_variants: productVariants,
       },
     };
   } catch (error) {
